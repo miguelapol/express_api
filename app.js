05b5d3ade0eaf624120a7ea45ad3c4e00eeccc55
miguelapol <apolonio1995@gmail.com>
2022-04-23 15:59:58 -0500

    AA2 creamos un endpoint para obtener los usuarios la cualÃ creamos varios objetos para luego obtener

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const port=3000;
+
+//creamos un endpoint para obtener todos los usuarios
+app.get('/v1/explorers', (req, res) => {
+    console.log(`Api explorers GET ALL request ${new Date()}`);
+    
+    const explorer1 = {
+        id: 1,
+        name: 'Explorer 1'
+    }
+    const explorer2 = {
+        id: 2,
+        name: 'Explorer 2'
+    }
+    const explorer3 = {
+        id: 3,
+        name: 'Explorer 3'
+    }
+    const explorers=[explorer1, explorer2, explorer3];
+    //aqui enviamos la respuesta usamos como codigo de estado 200
+
+    res.status(200).send(explorers);
+});
+
+
+
 
 //iniciamos el servidor
 app.listen(port,()=>{
